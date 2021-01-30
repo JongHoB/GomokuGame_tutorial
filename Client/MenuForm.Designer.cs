e11e8d388314ab3e9b36c1be9f69c80276b5eabe
qorwhdghjr <qorwhdghjr@naver.com>
2021-01-30 14:44:01 +0900

    Create GomokuGame Tutorial

---
+++ Client/MenuForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Client
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SinglePlaybutton = new System.Windows.Forms.Button();
+            this.multiPlayButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(703, 174);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(270, 249);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(114, 50);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "종료하기";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click_1);
+            // 
+            // SinglePlaybutton
+            // 
+            this.SinglePlaybutton.Location = new System.Drawing.Point(270, 140);
+            this.SinglePlaybutton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SinglePlaybutton.Name = "SinglePlaybutton";
+            this.SinglePlaybutton.Size = new System.Drawing.Size(114, 50);
+            this.SinglePlaybutton.TabIndex = 3;
+            this.SinglePlaybutton.Text = "혼자하기";
+            this.SinglePlaybutton.UseVisualStyleBackColor = true;
+            this.SinglePlaybutton.Click += new System.EventHandler(this.SinglePlaybutton_Click);
+            // 
+            // multiPlayButton
+            // 
+            this.multiPlayButton.Location = new System.Drawing.Point(270, 192);
+            this.multiPlayButton.Name = "multiPlayButton";
+            this.multiPlayButton.Size = new System.Drawing.Size(114, 50);
+            this.multiPlayButton.TabIndex = 4;
+            this.multiPlayButton.Text = "함께하기";
+            this.multiPlayButton.UseVisualStyleBackColor = true;
+            this.multiPlayButton.Click += new System.EventHandler(this.multiPlayButton_Click_1);
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 451);
+            this.Controls.Add(this.multiPlayButton);
+            this.Controls.Add(this.SinglePlaybutton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MenuForm";
+            this.Text = "Gomoku Game project";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button SinglePlaybutton;
+        private System.Windows.Forms.Button multiPlayButton;
+    }
+}
+
